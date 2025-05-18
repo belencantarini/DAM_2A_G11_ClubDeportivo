984d2e4369579d525cad1c084c3db1c23e4b5aac
Belen Cantarini <belencantarini@gmail.com>
2025-05-18 20:15:35 -0300

    perfil activity

--- app/src/main/java/com/example/clubdeportivog11/ClientProfileActivity.kt
+++ app/src/main/java/com/example/clubdeportivog11/ClientProfileActivity.kt
@@ -8,31 +8,22 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class ClientListActivity : AppCompatActivity() {
+class ClientProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_client_list)
+        setContentView(R.layout.activity_client_profile)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val btnVolver = findViewById<Button>(R.id.btnVolverBC)
+        val btnVolver = findViewById<Button>(R.id.btnVolverPC)
 
         btnVolver.setOnClickListener() {
             val intent = Intent(this, MenuActivity::class.java);
             startActivity(intent);
         }
-
-        val btnVer = findViewById<Button>(R.id.btnVerPC)
-
-        btnVer.setOnClickListener() {
-            val intent = Intent(this, ClientProfileActivity::class.java);
-            startActivity(intent);
-        }
-
-
     }
 }
--- app/src/main/java/com/example/clubdeportivog11/MembershipsActivity.kt
+++ app/src/main/java/com/example/clubdeportivog11/MembershipsActivity.kt
@@ -2,7 +2,9 @@ package com.example.clubdeportivog11
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -24,6 +26,37 @@ class MembershipsActivity : AppCompatActivity() {
         btnVolver.setOnClickListener() {
             val intent = Intent(this, MenuActivity::class.java);
             startActivity(intent);
+        }
+
+
+        val recuadroPlanes = findViewById<LinearLayout>(R.id.verPlanes)
+        val recuadroActividades = findViewById<LinearLayout>(R.id.verActividades)
+
+        val btnVerPlanes = findViewById<LinearLayout>(R.id.btnVerPlanes)
+
+        btnVerPlanes.setOnClickListener() {
+            recuadroPlanes.visibility = View.VISIBLE
+            recuadroActividades.visibility = View.GONE
+        }
+
+        val btnVerActividades = findViewById<LinearLayout>(R.id.btnVerActividades)
+
+        btnVerActividades.setOnClickListener() {
+            recuadroActividades.visibility = View.VISIBLE
+            recuadroPlanes.visibility = View.GONE
+        }
+
+        val btnCerrarPlanes = findViewById<Button>(R.id.btnCerrarPlanes)
+
+        btnCerrarPlanes.setOnClickListener() {
+
+            recuadroPlanes.visibility = View.GONE
+        }
+
+        val btnCerrarActividades = findViewById<Button>(R.id.btnCerrarActividades)
+
+        btnCerrarActividades.setOnClickListener() {
+            recuadroActividades.visibility = View.GONE
         }
 
     }
